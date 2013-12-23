85ef9761aaa6d15c3cd5889ca4a73c0e4673ff58
bocang <bocang@bocangtekiiMac.local>
2013-12-23 15:14:54 +0800

    Initial Commit

---
+++ YCComboBox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YCComboBox
+//
+//  Created by bocang on 13-12-23.
+//  Copyright (c) 2013年 yococo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YCComboBox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YCComboBox
+//
+//  Created by bocang on 13-12-23.
+//  Copyright (c) 2013年 yococo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
